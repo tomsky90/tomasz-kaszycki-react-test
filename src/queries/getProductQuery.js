3f0edfcef07b09cba9833d031ecd8e16377930d4
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-03-17 07:40:09 +0000

    replace single addField with addFieldList

--- src/queries/getProductQuery.js
+++ src/queries/getProductQuery.js
@@ -1,24 +1,25 @@
-import { Query,Field } from '@tilework/opus';
+import { Query, Field } from "@tilework/opus";
 
 const getProductQuery = (id) => {
-    const query = new Query('product')
-        .addArgument('id', 'String!', id)
-        .addField('name')
-        .addField('inStock')
-        .addField('description')
-        .addField( new Field ('gallery', true))
-        .addField('category')
-        .addField('id')
-        .addField( new Field ('attributes', true)
-            .addFieldList(['name', 'id', 'type'])
-                .addField( new Field('items', true)
-                .addFieldList(['displayValue', 'value', 'id'])))
-        .addField( new Field ('prices', true)
-            .addField('amount')
-            .addField(new Field ('currency', true)
-                .addFieldList(['label', 'symbol'])))
+  const query = new Query("product")
+    .addArgument("id", "String!", id)
+    .addFieldList(["name", "inStock", "description"])
+    .addField(new Field("gallery", true))
+    .addFieldList(["category", "id"])
+    .addField(
+      new Field("attributes", true)
+        .addFieldList(["name", "id", "type"])
+        .addField(
+          new Field("items", true).addFieldList(["displayValue", "value", "id"])
+        )
+    )
+    .addField(
+      new Field("prices", true)
+        .addField("amount")
+        .addField(new Field("currency", true).addFieldList(["label", "symbol"]))
+    );
 
-                return query
-}
+  return query;
+};
 
-export default getProductQuery
+export default getProductQuery;
